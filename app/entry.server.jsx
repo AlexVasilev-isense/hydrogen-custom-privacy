321a8bd90e53b1bfab715f9395e477f73ccf53b4
AlexVasilev-isense <alexvasilev@isenselabs.com>
2024-09-17 15:26:20 +0300

    test

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -18,8 +18,8 @@ export default async function handleRequest(
   context,
 ) {
   const {nonce, header, NonceProvider} = createContentSecurityPolicy({
-    connectSrc: ['https://consentmo-dev.com/users/versioning', 'https://consentmo-geo.com/users/checkIp', 'https://consentmo-dev.com/users/getCookieConsentSettings'],
-    scriptSrcElem: ["'self'",'https://cdn.shopify.com','https://consentmo-dev.com/webroot/js/solidjs/dist/bundle.js'],
+    connectSrc: ["'self'",'https://cdn.shopify.com/*', 'https://shopify.com', 'https://consentmo-dev.com/users/versioning', 'https://consentmo-geo.com/users/checkIp', 'https://consentmo-dev.com/users/getCookieConsentSettings'],
+    scriptSrcElem: ["'self'",'https://cdn.shopify.com/*', 'https://shopify.com', 'https://consentmo-dev.com/webroot/js/solidjs/dist/bundle.js'],
     shop: {
       checkoutDomain: context.env.PUBLIC_CHECKOUT_DOMAIN,
       storeDomain: context.env.PUBLIC_STORE_DOMAIN,
